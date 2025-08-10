6defecde957be312f45f9d15facb0e062881d76b
JJSax <5084938+JJSax@users.noreply.github.com>
2025-08-10 16:46:45 -0600

    Update ElementManager.lua
    Separates inBounds and activation code.
    Adds relative positioning based off of parent

--- apis/ElementManager.lua
+++ apis/ElementManager.lua
@@ -3,32 +3,86 @@ local ElementManagerMeta = {
 	-- __index metamethod allows you to define a fallback for accessing keys not present in the table
 	__index = ElementManager
 }
-ElementManager._VERSION = "0.4.0"
+ElementManager._VERSION = "0.4.4"
 
-function ElementManager:newElement()
-	local self = setmetatable({}, {
-		__index = self -- Children inherit properties and methods from the element manager
-	})
-	self.children = {}
-	return self
+function ElementManager:newElement(init)
+	local element = setmetatable(init or {}, { __index = ElementManager })
+	element.children = {}
+	return element
 end
 
 -- Adds child elements to an element
 function ElementManager:addChild(parent, ...)
-	for _, v in ipairs({...}) do
+	for _, v in ipairs({ ... }) do
+		v.parent = parent
 		table.insert(parent.children, v)
 	end
 end
 
+function ElementManager:removeChild(parent, child)
+	error("Not Implemented Yet")
+end
+
+--todo consider having method to hide/show; may need to redraw parent
+
 -- Function to draw all elements recursively
 function ElementManager:drawAll(element)
-	if element.skipDraw then return end
+	if element.hide then return end
 	if element.draw then
 		element:draw() -- Draw the element
 	end
 	for _, child in ipairs(element.children) do
 		self:drawAll(child) -- Draw all children recursively
 	end
+end
+
+function ElementManager:setPosition(x, y)
+	self.x = x
+	self.y = y
+end
+
+function ElementManager:getAbsolutePosition()
+	local x, y = self.x or 1, self.y or 1
+	local parent = self.parent
+	while parent do
+		x = x + (parent.x or 0)
+		y = y + (parent.y or 0)
+		parent = parent.parent
+	end
+	return x, y
+end
+
+function ElementManager:setCursorPos(relX, relY)
+	local absX, absY = self:getAbsolutePosition()
+	term.setCursorPos(absX + (relX - 1), absY + (relY - 1))
+end
+
+function ElementManager:inBounds(obj, x, y)
+	if obj.hide then return false end
+	for _, v in ipairs(obj.children) do
+		local inside = self:inBounds(v, x, y)
+		if inside then
+			return inside
+		end
+	end
+
+	if not obj.x or not obj.y or not obj.width or not obj.height then return false end
+
+	local absX, absY = obj:getAbsolutePosition()
+	if x >= absX and x < absX + obj.width and y >= absY and y < absY + obj.height then
+		return obj
+	end
+
+	return false
+end
+
+function ElementManager:triggerAt(obj, x, y, button)
+	local target = self:inBounds(obj, x, y)
+	if target and target.activate then
+		target:activate(button, x, y)
+		return true
+	end
+	return false
 end
 
 if false then -- make true if you want to run the demo
@@ -73,7 +127,6 @@ if false then -- make true if you want to run the demo
 	function subchild2:draw()
 		print("Drawing sub 2 of child 2")
 	end
-
 
 	-- Draw all elements
 	manager:drawAll(root)
